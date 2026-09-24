19c99e001ecc2669009f99bd9c1cbbf0341c0555
yrpanopio <yrpanopio@carc.pshs.edu.ph>
2026-09-24 08:37:48 +0800

    Add files via upload
    I added comments explaining the purpose of each segment for readability.

--- Panopio_Ysabelle_DistanceProgram.py
+++ Panopio_Ysabelle_DistanceProgram.py
@@ -1,13 +1,19 @@
 #Programmer: Ysabelle D. Panopio
 #Date: 08/18/2026
 import math
+# I imported a math library in order to make later calculations faster and easier.
 
 x1 = int(input("Enter x1: "))
 y1 = int(input("Enter y1: "))
 x2 = int(input("Enter x2: "))
 y2 = int(input("Enter y2: "))
+# Here, i ask the user for all the necessary numbers for the distance calculations. I used the variable type "integer" for this.
+
 d = math.sqrt((math.pow((x1-x2), 2)+(math.pow((y1-y2), 2))))
+# This is the calculation stage, I used the functions I imported from the math library to make the calculation easier and a bit more readable.
+
 print(f"The distance between the two points is: {d}")
+# Here, the algorithm gives the user the desired output.
 
 #Reflection:
 #Math libraries helped simply my program by simplifying otherwise complicated code into only a few operations.
